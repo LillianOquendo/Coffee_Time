64c24e97182fe18e0aeabad5df199fcd859ba3ed
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-06-29 15:18:49 -0400

    models and relationships updated

---
+++ lib/models.py
@@ -0,0 +1,68 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+
+
+##Unsure of imports
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+
+db = SQLAlchemy()
+
+
+
+class Coffee(db.Model):
+    __tablename__ = "coffees"
+
+    id = db.Column(db.Integer, primary_key=True)
+    coffeeshop_id = db.Column(db.Integer, ForeignKey('shops.id'))
+    name = db.Column(db.String)
+    roast = db.Column(db.String)
+    coarse_vs_fine = db.Column(db.Boolean)
+    price = db.Column(db.Numeric)
+
+##Migrations
+
+##creates a many to many relationship between coffee and bean tables
+    beans = relationship('Bean', secondary='coffee_beans', backref='coffees')
+
+class Bean(db.Model):
+    __tablename__ = "beans"
+
+    id = db.Column(db.Integer, primary_key=True)
+    bean_type = db.Column(db.String)
+    location = db.Column(db.String, ForeignKey('bean_locations.location'))
+    flavor_profile = db.Column(db.String)
+
+class Location(db.Model):
+    __tablename__ = "locations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String, unique=True)
+    shops = relationship('Shop', backref='location')
+    beans = relationship('Bean', backref='location')
+
+class Shop(db.Model):
+    __tablename__ = "shops"
+
+    id = db.Column(db.Integer, primary_key=True)
+    shop_name = db.Column(db.String)
+    shop_location = db.Column(db.String, ForeignKey('shop_locations.location'))
+    rating = db.Column(db.String)
+
+    
+##creates a one to many relationship between shop and coffee tables
+    coffees = relationship('Coffee', backref='shop')
+
+##JOIN table to create the many to many relationship between the coffee and bean tables
+
+coffee_beans = db.Table('coffee_beans',
+    db.Column('coffee_id', db.Integer, ForeignKey('coffees.id'), primary_key=True),
+    db.Column('bean_id', db.Integer, ForeignKey('beans.id'), primary_key=True)
+)
+
+
+
